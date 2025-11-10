14dbb51457a77bf40bf3c99f6efdf134d7f9188a
Darwing <darwingx990@hotmail.com>
2025-11-09 22:49:04 -0500

    Conection to DB Created

---
+++ config/databases.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewURLParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("✅ Connected to MongoDB. All is good to Go!");
+    } catch (error) {
+        console.error("❌ Error trying to connect to MongoDB; ", error.message);
+        process.exit(1);
+}
+};
+
+module.exports = connectDB;
+
+
